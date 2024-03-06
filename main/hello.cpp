973b38684eb2d8ba2133cf897c5941ad0c8bb440
Sharath-44 <sharath.shashidhara@gmail.com>
2024-03-06 13:28:46 +0530

    Change

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Changed something\n";
+  cout << "Changed somethings\n";
   return 0;
 }
